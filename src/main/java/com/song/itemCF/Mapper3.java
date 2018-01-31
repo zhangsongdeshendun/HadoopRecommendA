d8d3d2f2a5e9c788535589c3c51e9b3d32517e32
song.zhang <zhangs@inke.cn>
2018-01-31 16:26:07 +0800

    完成评分矩阵的转置

---
+++ src/main/java/com/song/itemCF/Mapper3.java
@@ -0,0 +1,40 @@
+package com.song.itemCF;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mapper3 extends Mapper<LongWritable, Text, Text, Text> {
+
+    private Text outKey = new Text();
+    private Text outValue = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String[] rowAndLine = value.toString().split("\t");
+
+        //矩阵的行号
+        String row = rowAndLine[0];
+        String[] lines = rowAndLine[1].split(",");
+
+        //["1_0","2_3","3_-1","4_2","5_-3"]
+        for (int i = 0; i < lines.length; i++) {
+            String column = lines[i].split("_")[0];
+            String valueStr = lines[i].split("_")[1];
+            //key:列号  value:行号_值
+            outKey.set(column);
+            outValue.set(row + "_" + valueStr);
+            context.write(outKey, outValue);
+        }
+
+
+    }
+}
