fb4ba1fe1905ebd07b2ce0468e7c7ebe7e2c418f
song.zhang <zhangs@inke.cn>
2018-02-05 15:24:09 +0800

    完成了基于内容的推荐算法

---
+++ src/main/java/com/song/base_content/Reduce1.java
@@ -0,0 +1,33 @@
+package com.song.base_content;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class Reduce1 extends Reducer<Text, Text, Text, Text> {
+
+    private Text outKey = new Text();
+    private Text outValue = new Text();
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+        StringBuilder sb = new StringBuilder();
+        for (Text value : values) {
+            //text:行号_值
+            sb.append(value + ",");
+        }
+        String result = null;
+        if (sb.toString().endsWith(",")) {
+            result = sb.substring(0, sb.length() - 1);
+        }
+
+        //outKey:行 outValue 列_值，列_值，列_值
+        outKey.set(key);
+        outValue.set(result);
+        context.write(outKey, outValue);
+
+
+    }
+}
---
+++ src/main/java/com/song/base_content/Reduce3.java
@@ -0,0 +1,32 @@
+package com.song.base_content;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class Reduce3 extends Reducer<Text, Text, Text, Text> {
+
+    private Text outKey = new Text();
+    private Text outValue = new Text();
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+        StringBuilder sb = new StringBuilder();
+        for (Text value : values) {
+            sb.append(value + ",");
+        }
+        String result = null;
+        if (sb.toString().endsWith(",")) {
+            result = sb.substring(0, sb.length() - 1);
+        }
+
+        //outKey:行 outValue 列_值，列_值，列_值
+        outKey.set(key);
+        outValue.set(result);
+        context.write(outKey, outValue);
+
+
+    }
+}
